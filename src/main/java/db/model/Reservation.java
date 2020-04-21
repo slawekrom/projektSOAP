f044538738ac715c6693f57801dbf9d56400a447
slawekrom <slawekrom@op.pl>
2020-04-21 14:39:13 +0200

    Utworzenie encji

---
+++ src/main/java/db/model/Reservation.java
@@ -0,0 +1,27 @@
+package db.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "RESERVATION")
+public class Reservation {
+
+    @Id
+    @GeneratedValue(generator="increment")
+    @Column(name = "ID_RES", nullable = false)
+    private int id_eservation;
+
+    @Column
+    private String places;
+    @Column
+    private Boolean isPaid;
+    @ManyToOne
+    @JoinColumn(name = "ID_PERSON")
+    private Person person;
+    @ManyToOne
+    @JoinColumn
+    private Showing showing;
+
+}
